4a1bda66a990dc7279c1b1de2e5707616cbe739e
Adayane Coutinho <adayanencl@gmail.com>
2021-06-07 17:47:16 -0300

    Projeto criado!

---
+++ src/main/java/com/adayanecoutinho/SpringModeloConceitual/SpringModeloConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.adayanecoutinho.SpringModeloConceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModeloConceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adayanecoutinho/SpringModeloConceitual/SpringModeloConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adayanecoutinho.SpringModeloConceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModeloConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
