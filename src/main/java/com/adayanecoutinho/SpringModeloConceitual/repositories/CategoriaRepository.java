a9f1fa262318134271e45eadd85c190749b67a2f
Adayane Coutinho <adayanencl@gmail.com>
2021-06-09 11:01:00 -0300

    Criando Repository e Service para categoria

---
+++ src/main/java/com/adayanecoutinho/SpringModeloConceitual/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.adayanecoutinho.SpringModeloConceitual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adayanecoutinho.SpringModeloConceitual.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
---
+++ src/main/java/com/adayanecoutinho/SpringModeloConceitual/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.adayanecoutinho.SpringModeloConceitual.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adayanecoutinho.SpringModeloConceitual.domain.Categoria;
+import com.adayanecoutinho.SpringModeloConceitual.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
