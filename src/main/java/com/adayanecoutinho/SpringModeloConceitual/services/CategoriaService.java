b78ec82739dda98c8f3bc467330069ee9e16e596
Adayane Coutinho <adayanencl@gmail.com>
2021-06-16 08:19:32 -0300

    Ajustes no endpoint /categorias

--- src/main/java/com/adayanecoutinho/SpringModeloConceitual/services/CategoriaService.java
+++ src/main/java/com/adayanecoutinho/SpringModeloConceitual/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.adayanecoutinho.SpringModeloConceitual.domain.Categoria;
 import com.adayanecoutinho.SpringModeloConceitual.repositories.CategoriaRepository;
+import com.adayanecoutinho.SpringModeloConceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,13 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrada. Id:" + id
+					+ ", tipo:" + Categoria.class.getName());
+		}
+		
+		
 		return obj.orElse(null);
 	}
 
