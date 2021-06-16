e91cf6b538e5b61b6c93699bed94a955456122fd
Adayane Coutinho <adayanencl@gmail.com>
2021-06-16 13:59:52 -0300

    Implementação de Cliente e Endereço

--- src/main/java/com/adayanecoutinho/SpringModeloConceitual/domain/enums/TipoCliente.java
+++ src/main/java/com/adayanecoutinho/SpringModeloConceitual/domain/enums/TipoCliente.java
@@ -2,7 +2,38 @@ package com.adayanecoutinho.SpringModeloConceitual.domain.enums;
 
 public enum TipoCliente {
 	
-	PESSOAFISICA,
-	PESSOAJURIDICA
+	PESSOAFISICA(1,"Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
 	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String descricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}			
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+		
+	}
 }
